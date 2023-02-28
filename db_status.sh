3d467428c971fcbe3abced65fc6d8929fbffcd3d
Yuji Sakaguchi <Yuji.Sakaguchi1@ibm.com>
2023-02-28 10:02:45 +0900

    微修正

---
+++ db_status.sh
@@ -0,0 +1,9 @@
+#mongo db 
+echo "mongodb status"
+brew services info mongodb-community
+sleep 1
+echo "=========================================="
+
+# mysql
+echo "My SQL server status"
+mysql.server status
