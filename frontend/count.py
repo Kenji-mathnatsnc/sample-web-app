e2e5b92b664eea9ecd5d3639fb59399910b93fd1
Yuji Sakaguchi <Yuji.Sakaguchi1@ibm.com>
2023-02-28 16:11:15 +0900

    メニュー一覧を定義ファイル化

--- frontend/count.py
+++ frontend/count.py
@@ -1,4 +1,10 @@
-from view_selector import ViewSelector
+import json
+import pandas as pd
 
-selector: ViewSelector = ViewSelector('aaa')
-print(selector.strategy)
+# clazz_names = pd.read_csv("./config/menu.csv").to_dict()
+with open("./config/menu.csv", mode="r", encoding="UTF-8") as f:
+    s = f.read()
+
+dict_data = json.loads(s)
+
+print(list(dict_data.keys()))
--- frontend/user_viewer.py
+++ frontend/user_viewer.py
@@ -1,12 +1,16 @@
+import json
 import streamlit as st
 
 from view_selector import ViewSelector
 
+with open("./config/menu.csv", mode="r", encoding="UTF-8") as f:
+    s = f.read()
+
+menu_list = list(json.loads(s).keys())
 
 st.title("Streamlit サンプル")
 
-page = st.sidebar.selectbox(
-    'メニューを選択せよ', ['home', 'register', 'inquire', 'inquireAll', 'update', 'deleteAll'])
+page = st.sidebar.selectbox('メニューを選択せよ', menu_list)
 
 view_selector: ViewSelector = ViewSelector(page)
 view_selector.perform_selected_logic()
